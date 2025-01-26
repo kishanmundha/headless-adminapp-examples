3acb03a76f77d0a0eff6a9fcde9485bef83034f0
kishanmundha <kishan.mundha@gmail.com>
2025-01-26 14:21:00 +0900

    Add Github icon and link to help desk app

--- app/help-desk/config/apps/index.ts
+++ app/help-desk/config/apps/index.ts
@@ -2,6 +2,7 @@ import { AppExperience } from '@headless-adminapp/core/experience/app';
 import { navPageGroupAreas } from './navigations';
 import { commands } from '../commands';
 import { bundleLazyIcon } from '@headless-adminapp/icons-fluent/lazyIcon';
+import { GithubIcon } from '@/app/icons/Github';
 
 const SettingsIcon = bundleLazyIcon('Settings24Regular', 'Settings24Filled');
 const AlertIcon = bundleLazyIcon('Alert24Regular', 'Alert24Filled');
@@ -27,6 +28,17 @@ export const defaultApp: AppExperience = {
     {
       label: 'Settings',
       icon: SettingsIcon,
+    },
+    {
+      label: 'Github',
+      icon: GithubIcon,
+      link: 'https://github.com/kishanmundha/headless-adminapp-examples',
+      onClick: () => {
+        window.open(
+          'https://github.com/kishanmundha/headless-adminapp-examples',
+          '_blank'
+        );
+      },
     },
   ],
   viewCommands: commands.view,
