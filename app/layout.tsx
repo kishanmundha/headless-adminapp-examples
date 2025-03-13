ed6c41a7bcb48283bb05aaf3a01942785992b4dc
kishanmundha <kishan.mundha@gmail.com>
2025-03-14 08:19:20 +0900

    Conditionally load Clarity script based on environment to improve performance in development

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import Script from 'next/script';
 import './styles.css';
 
+const isDev = process.env.NODE_ENV === 'development';
+
 export const metadata = {
   title: 'Headless AdminApp',
   description: 'Headless AdminApp',
@@ -13,9 +15,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <head>
-        <Script src="/clarity.js" />
-      </head>
+      <head>{!isDev && <Script src="/clarity.js" />}</head>
       <body>{children}</body>
     </html>
   );
